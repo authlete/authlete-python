d2cb769ff80be38a740e6d37695a46adc295f3ca
Takahiko Kawasaki <taka@authlete.com>
2019-08-09 18:09:37 +0900

    Modified setup.py to update the version to 1.0.2.

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="authlete",
-    version="1.0.1",
+    version="1.0.2",
     license="Apache License, Version 2.0",
     author="Authlete, Inc.",
     author_email="admin@authlete.com",
